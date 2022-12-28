187c1a4fe97b2eccf50dc56dbee14b7970880ec0
Huda4000 <hudatabassum400@gmail.com>
2022-12-28 21:50:04 +0530

    ADDED: to check employee's monthly wage

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -5,28 +5,33 @@ public class EmployeeWage {
 		
 		System.out.println("Welcome to employee wage computation");
 		int Wage_Per_Hr = 20;                    
-		int Working_Hr = 8;                      
-		double dailyWage;    
+		int Working_Hr;                      
+		double dailyWage =0;    
 		int Part_time_hr = 4;
+		int workingDaysPerMonth = 20;
+		double monthlyWage = 0;
+		for(int day = 0; day < workingDaysPerMonth; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case 1:
+                    System.out.println("Employee is Present");
+                    Working_Hr= 8;
+                    break;
+                case 2:
+                    System.out.println("Employee is Part-Time Present");
+                    Working_Hr = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    Working_Hr= 0;
+            }
+            dailyWage = Working_Hr * Wage_Per_Hr;
+            monthlyWage = dailyWage + monthlyWage;
+            System.out.println("Daily wage of employee is "+ dailyWage);
+        }
 		
-		double empCheck = Math.floor(Math.random()*10)%3; 
-		if(empCheck == 1)                                 
-		{
-			System.out.println("Employee is present");   
-			dailyWage = Wage_Per_Hr * Working_Hr;     
-			System.out.println(dailyWage);  
-		}
-	
-		else if (empCheck == 2)
-		{
-			System.out.println("Employee's part time");
-			dailyWage = Wage_Per_Hr * Part_time_hr;     
-			System.out.println(dailyWage);  
-
-		}
-		else {
-			System.out.println("Employee is absent");   
-		}
+		System.out.println("-------------------");
+        System.out.println("Monthly employee wage is "+ monthlyWage);
 	}
 
 }
